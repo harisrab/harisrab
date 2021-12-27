48d6a2634535c3beb48d0021101ec8443bcafc3a
unknown <harisrab01@gmail.com>
2021-12-27 22:36:25 +0800

    Blur Header

--- components/Common/Header.js
+++ components/Common/Header.js
@@ -12,8 +12,8 @@ function Header() {
 	}, [isMenuVisible]);
 
 	return (
-		<div className="w-screen h-[60px] flex items-center px-5 pl-0 bg-[#0B0909] border-b-[1px] border-opacity-10 border-white fixed top-0 left-0 z-50 filter drop-shadow-2xl bg-opacity-90 md:px-6 ">
-			<div className="w-full h-full flex items-center justify-between relative">
+		<div className="w-screen h-[60px] flex items-center px-5 pl-0 bg-[#0B0909] border-b-[1px] border-opacity-10 border-white fixed top-0 left-0 z-50 filter drop-shadow-2xl bg-opacity-90 md:px-6 filter backdrop-blur-sm">
+			<div className="w-full h-full flex items-center justify-between relative ">
 				<Logo />
 				{/* <BurgerMenu
 					isMenuVisible={isMenuVisible}

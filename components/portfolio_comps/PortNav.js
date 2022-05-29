e5d0837af7462065ec9a81b896213f331e736e4f
harisrab <harisrab01@gmail.com>
2022-05-30 02:04:50 +0500

    Deployed Changes to Portfolio Site

--- components/portfolio_comps/PortNav.js
+++ components/portfolio_comps/PortNav.js
@@ -3,12 +3,12 @@ import Link from 'next/link'
 
 function PortNav() {
   return (
-                <div className="flex items-center justify-between px-14 w-screen h-[8%] z-[1000] bg-[#000000a7] border-t-[#ffffff38] border-b-[#ffffff38] border-t-[1px] border-b-[1px] border-opacity-10 backdrop-blur-sm b-0">
-                <div className='w-full flex items-center justify-start h-full border-r-[#ffffff38] border-r-[1px] border-opacity-10'>
-                <h1 className="text-white font-sans uppercase text-[12px] tracking-[5px] hover:opacity-60 duration-300 hover:cursor-pointer">Haris Rashid</h1>
+                <div className="flex items-center justify-between flex-col md:flex-row px-14 w-screen h-[40%] py-10 md:py-0 md:h-[8%] z-[1000] bg-[#000000a7] border-t-[#ffffff38] border-b-[#ffffff38] border-t-[1px] border-b-[1px] border-opacity-10 backdrop-blur-sm b-0">
+                <div className='w-full flex items-center justify-center h-full  border-r-[#ffffff38] md:border-r-[1px] md:border-opacity-10'>
+                  <h1 className="text-white font-sans uppercase text-[12px] tracking-[5px] hover:opacity-60 duration-300 hover:cursor-pointer">Haris Rashid</h1>
                 </div> 
-                  <div className="w-full h-full pl-10">
-                    <ul className='text-white font-sans text-[12px] tracking-[5px] uppercase h-full w-full flex items-center justify-between hover:cursor-pointer'>
+                  <div className="w-full h-full md:pl-10">
+                    <ul className='text-white font-sans text-[12px] tracking-[5px] uppercase h-full w-full flex items-center justify-between flex-col md:flex-row  hover:cursor-pointer'>
                     
                     <Link href="/">
                       <li className='hover:opacity-60 duration-300'>Work</li>
--- components/portfolio_comps/ProjectThumb.js
+++ components/portfolio_comps/ProjectThumb.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Image from 'next/image'
 
-function ProjectThumb({bg}) {
+function ProjectThumb({type, bg, title, desc, stack, deployment, github}) {
+  const [stackItems, setStackItems] = useState([]);
+
+  useEffect(() => {
+    // Convert an array to string
+    const items = stack.join(', ');
+    setStackItems(items);
+  }, [stack])
   return (
-        <div className='relative h-[400px] w-full bg-yellow-400'>
-            <Image priority layout="fill" src={bg} className='h-full w-full absolute t-0 l-0 object-cover'/>
-            <div className='h-full w-full absolute t-0 l-0 bg-black opacity-20 hover:cursor-pointer hover:opacity-80 duration-300'></div>
+        <div className='relative h-[400px] w-full md:w-[600px] bg-yellow-400 text-white flex flex-col  md:relative'>
+            <Image priority layout="fill" src={bg} className='h-[50%] md:h-full w-full md:absolute t-0 l-0 object-fill md:object-cover'/>
+            <div className='uppercase font-sans text-[12px] tracking-[3px] flex flex-col justify-between p-8 h-full w-full md:absolute md:t-0 md:l-0 bg-black opacity-0 hover:opacity-80 duration-300'>
+              <div>
+                  <p className=''>{type}</p>
+                  <h1 className='text-[20px] tracking-[5px] font-bold my-4'>{title}</h1>
+                  <p className='normal-case tracking-[0.5px] text-[14px] mb-4'>{desc}</p>
+                  <p className='capitalize tracking-[2px] mb-4'><span className='font-bold'>Stack: </span>{stackItems}</p>
+              </div>  
+              <div>
+              {deployment && (
+                  <p className='normal-case mb-4 tracking-[2px]'>Deployment: <a target="_blank" href={deployment} className="underline text-gray-400 hover:text-gray-500 duration-200">{deployment}</a></p>
+              )}
+              {github && (
+                  <p className='normal-case tracking-[2px]'>Github: <a target="_blank" href={github} className="underline text-gray-400 hover:text-gray-500 duration-200">{github}</a></p>
+              )}
+              </div>
+            </div>
 
             </div>
 
